36c35b993f9ec256d7291275c6926f3ad553ef08
diana1m <diana.maliarchuk.work@gmail.com>
2023-04-28 12:53:44 +0000

    Deploying to gh-pages from @ diana1m/goit-react-hw-08-phonebook@592d9ca81cf573f2d077b74df30750da7e219038 🚀

---
+++ static/js/266.2a3bb251.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"PhoneBook welcome page"})})}}}]);
+//# sourceMappingURL=266.2a3bb251.chunk.js.map
